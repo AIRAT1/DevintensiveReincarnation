8e465e3fdd90f6e76ae75ae6f901cd3c51cd689b
AIRAT1 <ayrat1@mail.ru>
2016-09-29 16:29:29 +0300

    2 00.55.10

--- app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
@@ -2,14 +2,24 @@ package de.android.testtodeletedevintensivereincarnation.ui.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import de.android.testtodeletedevintensivereincarnation.R;
+import de.android.testtodeletedevintensivereincarnation.utils.ConstantManager;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        if (savedInstanceState == null) {
+
+        }else {
+
+        }
     }
 }
---
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/utils/ConstantManager.java
@@ -0,0 +1,5 @@
+package de.android.testtodeletedevintensivereincarnation.utils;
+
+public class ConstantManager {
+    public static final String TAG_PREFIX = "DEV ";
+}
