6e6cbf6e1201c0c673daf63f3e0490a5c4acdd03
AIRAT1 <ayrat1@mail.ru>
2016-10-03 16:44:29 +0300

    03 00.43.46

--- app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
@@ -1,21 +1,22 @@
 package de.android.testtodeletedevintensivereincarnation.ui.activities;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 import de.android.testtodeletedevintensivereincarnation.R;
 import de.android.testtodeletedevintensivereincarnation.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
+    private ImageView callImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate");
+        callImg = (ImageView)findViewById(R.id.call_img);
+        callImg.setOnClickListener(this);
 
         if (savedInstanceState == null) {
             // активность запускается впервые
@@ -26,11 +27,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
-
+        switch (view.getId()) {
+            case R.id.call_img:
+//                showProgress();
+//                runWithDelay();
+                break;
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
+//    private void runWithDelay() {
+//        final Handler handler = new Handler();
+//        handler.postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                hideProgress();
+//            }
+//        }, 5000);
+//    }
 }
