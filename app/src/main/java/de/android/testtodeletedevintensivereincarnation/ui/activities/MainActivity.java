255802f085cbfaeb755354a2cebfbd88fd5783be
Ayrat <ayrat1@mail.ru>
2016-10-03 23:51:34 +0300

    03 01.06.13

--- app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
@@ -1,6 +1,10 @@
 package de.android.testtodeletedevintensivereincarnation.ui.activities;
 
 import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ImageView;
 
@@ -10,13 +14,18 @@ import de.android.testtodeletedevintensivereincarnation.utils.ConstantManager;
 public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
     private ImageView callImg;
+    private CoordinatorLayout coordinatorLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
         callImg = (ImageView)findViewById(R.id.call_img);
         callImg.setOnClickListener(this);
+        setupToolbar();
 
         if (savedInstanceState == null) {
             // активность запускается впервые
@@ -29,8 +38,6 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.call_img:
-//                showProgress();
-//                runWithDelay();
                 break;
         }
     }
@@ -39,13 +46,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
-//    private void runWithDelay() {
-//        final Handler handler = new Handler();
-//        handler.postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                hideProgress();
-//            }
-//        }, 5000);
-//    }
+    private void showSnackbar(String message) {
+        Snackbar.make(coordinatorLayout, message, Snackbar.LENGTH_LONG).show();
+    }
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 }
