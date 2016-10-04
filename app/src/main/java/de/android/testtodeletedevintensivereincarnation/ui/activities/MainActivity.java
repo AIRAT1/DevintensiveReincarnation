a876ded8554100fad7d06817ec5db80f5d29dcff
Ayrat <ayrat1@mail.ru>
2016-10-05 00:38:13 +0300

    03 01.30.46

--- app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/MainActivity.java
@@ -2,6 +2,8 @@ package de.android.testtodeletedevintensivereincarnation.ui.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -9,17 +11,27 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import de.android.testtodeletedevintensivereincarnation.R;
 import de.android.testtodeletedevintensivereincarnation.utils.ConstantManager;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener{
     private static final String TAG = ConstantManager.TAG_PREFIX + MainActivity.class.getSimpleName();
+
+    private int currentEditMode = 0;
+
     private ImageView callImg;
     private CoordinatorLayout coordinatorLayout;
     private Toolbar toolbar;
     private DrawerLayout navigationDrawer;
+    private FloatingActionButton fab;
+    private EditText userPhone, userMail, userVk, userGit, userBio;
+    private List<EditText> userInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,15 +40,31 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
         coordinatorLayout = (CoordinatorLayout)findViewById(R.id.main_coordinator_container);
         toolbar = (Toolbar)findViewById(R.id.toolbar);
         navigationDrawer = (DrawerLayout)findViewById(R.id.navigation_drawer);
-
+        fab = (FloatingActionButton)findViewById(R.id.fab);
         callImg = (ImageView)findViewById(R.id.call_img);
-        callImg.setOnClickListener(this);
+        userPhone = (EditText)findViewById(R.id.phone_et);
+        userMail = (EditText)findViewById(R.id.email_et);
+        userVk = (EditText)findViewById(R.id.vk_et);
+        userGit = (EditText)findViewById(R.id.github_et);
+        userBio = (EditText)findViewById(R.id.about_et);
+
+        userInfo = new ArrayList<>();
+        userInfo.add(userPhone);
+        userInfo.add(userMail);
+        userInfo.add(userVk);
+        userInfo.add(userGit);
+        userInfo.add(userBio);
+
+        fab.setOnClickListener(this);
         setupToolbar();
+        setupDrawer();
 
         if (savedInstanceState == null) {
             // активность запускается впервые
         }else {
             // активность уже создавалась
+            currentEditMode = savedInstanceState.getInt(ConstantManager.EDIT_MODE_KEY, 0);
+            changeEditMode(currentEditMode);
         }
     }
 
@@ -51,7 +79,15 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
-            case R.id.call_img:
+            case R.id.fab:
+                showSnackbar("click");
+                if (currentEditMode == 0) {
+                    changeEditMode(1);
+                    currentEditMode = 1;
+                }else {
+                    changeEditMode(0);
+                    currentEditMode = 0;
+                }
                 break;
         }
     }
@@ -59,6 +95,7 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
+        outState.putInt(ConstantManager.EDIT_MODE_KEY, currentEditMode);
     }
     @SuppressWarnings("unused")
     private void showSnackbar(String message) {
@@ -71,5 +108,43 @@ public class MainActivity extends BaseActivity implements View.OnClickListener{
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+    }
+    private void setupDrawer() {
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                showSnackbar(item.getTitle().toString());
+                item.setChecked(true);
+                navigationDrawer.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    /**
+     * переключает режим редактирования
+     * @param mode если 1 режим редактирования, если 0 режим просмотра
+     */
+    private void changeEditMode(int mode) {
+        if (mode == 1) {
+            for (EditText userValue : userInfo) {
+                userValue.setEnabled(true);
+                userValue.setFocusable(true);
+                userValue.setFocusableInTouchMode(true);
+            }
+        }else {
+            for (EditText userValue : userInfo) {
+                userValue.setEnabled(false);
+                userValue.setFocusable(false);
+                userValue.setFocusableInTouchMode(false);
+            }
+        }
+    }
+    private void loadUserInfoValue() {
+
+    }
+    private void saveUserInfoValue() {
+
     }
 }
--- app/src/main/java/de/android/testtodeletedevintensivereincarnation/utils/ConstantManager.java
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/utils/ConstantManager.java
@@ -2,4 +2,5 @@ package de.android.testtodeletedevintensivereincarnation.utils;
 
 public class ConstantManager {
     public static final String TAG_PREFIX = "DEV ";
+    public static final String EDIT_MODE_KEY = "EDIT_MODE_KEY";
 }
