5551874ec09e2633037bed9ac86c419cb5337a85
Ayrat <ayrat1@mail.ru>
2016-10-10 00:48:53 +0300

    05 00.53.38

---
+++ app/src/main/java/de/android/testtodeletedevintensivereincarnation/ui/activities/AuthActivity.java
@@ -0,0 +1,15 @@
+package de.android.testtodeletedevintensivereincarnation.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import de.android.testtodeletedevintensivereincarnation.R;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+    }
+}
